6dc3b0dc354a36bd07ce0eea02b9c33bb9e8816f
Aldrin Navarro <aldrinnavarro16@gmail.com>
2019-06-01 21:41:25 +0800

    Add more documentation

--- docs/usage.rst
+++ docs/usage.rst
@@ -5,3 +5,24 @@ Usage
 To use Geohash in a project::
 
     import geohash
+
+* Encode::
+
+    # Get geohash given latitude and longitude. Precision is optional.
+    geohash.geohash_encode(latitude=48.856614, longitude=2.352222, precision=6)
+    >> u09tvw
+
+  Takes in latitude and longitude with a desired precision and returns the correct hash value. If precision < 0 or
+  precision > 20, a default value of 6 will be used.
+
+* Decode::
+
+    # Get latitude, longitude and information about bounding box too!
+    korea = geohash.geohash_decode(hash_code="wy7b1h")
+    korea.latitude
+    korea.longitude
+    korea.north, korea.east, korea.south, korea.west
+    korea.dimension.height, korea.dimension.width
+
+  Returns GeoCoord structure which contains the latitude and longitude that was decoded from the geohash.
+  A GeoCoord also provides the bounding box for the geohash.
--- geohash/geohash.py
+++ geohash/geohash.py
@@ -5,9 +5,14 @@ from .geohash_build import ffi
 from .geohash_build import geohash_lib as lib
 
 
-def geohash_encode(lat: float, long: float, precision: int = -1):
-    return ffi.string(lib.geohash_encode(lat, long, precision)).decode()
+def geohash_encode(latitude: float, longitude: float, precision: int = -1):
+    """Takes in latitude and longitude with a desired precision and returns the correct hash value.
+    If precision < 0 or precision > 20, a default value of 6 will be used."""
+    return ffi.string(lib.geohash_encode(latitude, longitude, precision)).decode()
 
 
 def geohash_decode(hash_code: str):
+    """Returns GeoCoord structure which contains the latitude and longitude
+    that was decoded from the geohash. A GeoCoord also provides the bounding box for the
+    geohash (north, east, south, west, dimension.height, dimension.width)."""
     return lib.geohash_decode(hash_code.encode())
